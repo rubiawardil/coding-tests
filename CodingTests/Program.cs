6dcd759091b22de37d3d9b6024842c9d98d0efd5
Rúbia Wardil <wardil.rubia@gmail.com>
2023-05-22 17:10:04 -0300

    add project

---
+++ CodingTests/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problems descriptions and solutions can be found in 'Tests' folder");
+        }
+    }
+}
